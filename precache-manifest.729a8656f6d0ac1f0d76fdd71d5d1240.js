a53c36b4774f9e99f5b496fb16916ad585f57d8a
Yaroslav Frolov <noodles20@ukr.net>
2020-03-24 23:34:21 +0200

    Updates

---
+++ precache-manifest.729a8656f6d0ac1f0d76fdd71d5d1240.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45e27e65e29bc75163ffbe402b788d0",
+    "url": "/accounting/index.html"
+  },
+  {
+    "revision": "6ace1cdce40721628a7c",
+    "url": "/accounting/static/css/main.99eb531e.chunk.css"
+  },
+  {
+    "revision": "cd56c09b48ebaf73f500",
+    "url": "/accounting/static/js/2.de8ed63e.chunk.js"
+  },
+  {
+    "revision": "33f4409ce65456b8bf6a163a31ba5e0e",
+    "url": "/accounting/static/js/2.de8ed63e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ace1cdce40721628a7c",
+    "url": "/accounting/static/js/main.7ef64a40.chunk.js"
+  },
+  {
+    "revision": "91864d8e09e96156cd27",
+    "url": "/accounting/static/js/runtime-main.019dcc02.js"
+  },
+  {
+    "revision": "d7f5bd79ff28c387b2549cc167a239df",
+    "url": "/accounting/static/media/bg.d7f5bd79.jpeg"
+  },
+  {
+    "revision": "6f27c8d0d83a9ad569a98f1c6f7fd0ca",
+    "url": "/accounting/static/media/heartIcon.6f27c8d0.svg"
+  },
+  {
+    "revision": "531e4a9af31d5ccebec3d2244a4e0ab5",
+    "url": "/accounting/static/media/input__Argix_4Q_2019.531e4a9a.xlsx"
+  },
+  {
+    "revision": "80cd347dc6b669a860ea7525acaa946b",
+    "url": "/accounting/static/media/output__Argix_Declaration_4Q .80cd347d.xlsx"
+  }
+]);
